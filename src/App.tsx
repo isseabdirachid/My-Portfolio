2b74df29dbea2a7e021255673e4d5185d2969707
isseabdirachid <isseabdirachid@gmail.com>
2026-08-20 14:44:43 +0200

    fix vercel error

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h2 className="bg-amber-400">Hell </h2>
-        <Button className="bg-amber-400 shimmer-color-amber-50 m-4 p16 rounded-2xl">Click Me</Button>
+
     </>
   )
 }
