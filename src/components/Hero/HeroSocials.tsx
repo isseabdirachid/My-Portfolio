a43c553e289476e8af9925954332776ee06e8de5
isseabdirachid <isseabdirachid@gmail.com>
2026-08-25 11:25:34 +0200

    feat: make hero section responsive

--- src/components/Hero/HeroSocials.tsx
+++ src/components/Hero/HeroSocials.tsx
@@ -30,17 +30,13 @@ function HeroSocials() {
                         key={social.name}
                         href={social.href}
                         target={
-                            social.name === "Email"
-                                ? undefined
-                                : "_blank"
+                            social.name === "Email" ? undefined : "_blank"
                         }
                         rel={
-                            social.name === "Email"
-                                ? undefined
-                                : "noreferrer"
+                            social.name === "Email" ? undefined : "noreferrer"
                         }
                         aria-label={social.name}
-                        className="flex size-10 items-center justify-center rounded-full border border-primary-custom/50 text-primary-custom transition-all hover:border-primary-custom/30 hover:bg-primary-custom/30 hover:text-primary-custom md:-translate-x-32"
+                        className="flex size-10 items-center justify-center rounded-full border border-primary-custom/50 text-primary-custom transition-all hover:border-primary-custom/30 hover:bg-primary-custom/30 hover:text-primary-custom md:-translate-x-22"
                     >
                         <Icon className="size-4" />
                     </a>
