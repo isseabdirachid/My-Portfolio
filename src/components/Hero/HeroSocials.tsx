752539d378ca7c5c18932c5d1d0c94162f160bf9
isseabdirachid <isseabdirachid@gmail.com>
2026-08-25 11:41:15 +0200

    fix: adjust hero responsive layout and social icons

--- src/components/Hero/HeroSocials.tsx
+++ src/components/Hero/HeroSocials.tsx
@@ -21,7 +21,7 @@ const socials = [
 
 function HeroSocials() {
     return (
-        <div className="mt-4 flex items-center gap-3">
+        <div className="mt-4 flex  items-center gap-3 lg:-translate-x-8">
             {socials.map((social) => {
                 const Icon = social.icon;
 
@@ -36,7 +36,7 @@ function HeroSocials() {
                             social.name === "Email" ? undefined : "noreferrer"
                         }
                         aria-label={social.name}
-                        className="flex size-10 items-center justify-center rounded-full border border-primary-custom/50 text-primary-custom transition-all hover:border-primary-custom/30 hover:bg-primary-custom/30 hover:text-primary-custom md:-translate-x-22"
+                        className="flex size-10 items-center  justify-center rounded-full border border-primary-custom/50 text-primary-custom transition-all hover:border-primary-custom/30 hover:bg-primary-custom/30 hover:text-primary-custom md:-translate-x-22"
                     >
                         <Icon className="size-4" />
                     </a>
