07e70f18298b770ab6f8369077ac74e4601aa88b
isseabdirachid <isseabdirachid@gmail.com>
2026-08-25 13:10:43 +0200

    Adjust About section spacing and responsive layout

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,12 +5,16 @@ function About() {
     return (
         <section
             id="about"
-            className="w-full py-20 sm:py-24 lg:py-28"
+            className="w-full sm:py-1 lg:py-20"
         >
             <div className="mx-auto grid max-w-6xl grid-cols-1 items-center gap-8 px-6 sm:gap-10 lg:grid-cols-2 lg:gap-20">
-                <AboutImage />
+                <div className="order-2 sm:order-1">
+                    <AboutImage />
+                </div>
 
-                <AboutContent />
+                <div className="order-1 sm:order-2">
+                    <AboutContent />
+                </div>
             </div>
         </section>
     );
